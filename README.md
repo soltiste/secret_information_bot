319b57c0756320b9c41c9f4335f7f8144154d9c4
Your Name <ekirichenko2344@mail.ru>
2023-07-21 00:52:19 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# secret_information_bot
---
+++ main.py
@@ -0,0 +1,42 @@
+#код самого бота
+#в файле secret_information хранятся переменная TOKEN с токеном бота
+#и словарь MY_DICT c ключами /vk, /tg и /mail c соответствующими значениями(
+#строками с ссылками)
+from aiogram import Bot, Dispatcher, executor, types
+import secret_information as s_i
+
+
+HELP_COMMAND = """
+/start - начать работу с ботом
+/help - список комманд
+/vk - ссылка на вк
+/tg - ссылка на тг
+/mail - адрес почты"""
+
+bot = Bot(s_i.TOKEN)
+dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print()
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer(text='<em>Добро пожаловать!</em>', parse_mode="HTML")#ответ на сообщение
+    await message.delete()
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)#ответ на сообщение
+
+
+@dp.message_handler(commands=['vk', 'tg', 'mail'])
+async def ans(message: types.Message):
+    await message.answer(text=s_i.MY_DICT[message.text])
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_startup)#типо запуск бота
+
+
